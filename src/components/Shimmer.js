611170d879fa8a5e29cd1c8e00c6353172bdf780
sravaninaalam <sravaninaalam2000@gmail.com>
2023-10-04 14:10:59 +0530

    commit

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,8 +1,10 @@
 const Shimmer = () => {
     return (
+      
       <div className=' ml-4   flex flex-wrap'>
         {Array(20).fill("").map((e,index)=><div key={index} className='w-72 h-52 rounded-md bg-gray-200 m-4'></div>)}
       </div>
+      
     )
   }
   
