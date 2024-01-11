1076e5b2d982c806118b31dc648608a4a0b56e3a
sravaninaalam <sravaninaalam2000@gmail.com>
2024-01-11 11:40:34 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     const[open,setOpen]=useState(false)
   return (
     <>
-        <div className='bg-violet-300 shadow-sm md:flex  md:justify-between sticky top-0'>
+        <div className='bg-violet-300 w-full shadow-sm md:flex  md:justify-between sticky top-0'>
             <div className='flex justify-between items-center'>
             
               <Link to='/'><img src={LOGO_URL}className='h-20 py-3 rounded-full'/></Link>
@@ -19,7 +19,7 @@ const Header = () => {
                     {open?<X className='ml-7 md:hidden'/>:<AlignJustify className='ml-7 md:hidden'/>}
                 </div>
              </div>
-             <ul className={`md:flex items-center absolute md:static bg-violet-300 w-full md:w-auto
+             <ul className={` md:flex items-center absolute md:static  w-full md:w-auto
               ${open?'top-20 opacity-100':'top-[-490px]'}
              `}>
                <Link to='/'><li className='mx-4 my-4 md:my-0 font-medium  hover:text-pink-500 flex'>
